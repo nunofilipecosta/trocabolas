36af8651aefdb85d32d7d98cf89c4ddd82a26f42
Nuno Costa <nunofilipe.costa@gmail.com>
2014-07-31 11:53:34 +0200

    CHANGED : Publish Profile

---
+++ TrocaBolas.Database/ReferenceData/CustomerType.Data.sql
@@ -0,0 +1,7 @@
+﻿DELETE FROM CustomerType
+
+SET IDENTITY_INSERT [dbo].[CustomerType] ON
+INSERT INTO [dbo].[CustomerType] ([CustomerTypeId], [Description]) VALUES (1, N'Corporation')
+INSERT INTO [dbo].[CustomerType] ([CustomerTypeId], [Description]) VALUES (2, N'Individual')
+INSERT INTO [dbo].[CustomerType] ([CustomerTypeId], [Description]) VALUES (3, N'Educator')
+SET IDENTITY_INSERT [dbo].[CustomerType] OFF
